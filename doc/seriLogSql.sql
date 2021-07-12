5a1be9c820667c46e364a794379a53b11b61b200
Jadran Grba <jadrangrba@gmail.com>
2021-07-12 17:33:15 +0200

    adds serlog db script

---
+++ doc/seriLogSql.sql
@@ -0,0 +1,24 @@
+USE [BookCatalogWebApi]
+GO
+/****** Object:  Table [dbo].[Log]    Script Date: 26.6.2021. 14:13:21 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Log](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Message] [nvarchar](max) NULL,
+	[MessageTemplate] [nvarchar](max) NULL,
+	[Level] [nvarchar](128) NULL,
+	[TimeStamp] [datetimeoffset](7) NOT NULL,
+	[Exception] [nvarchar](max) NULL,
+	[Properties] [xml] NULL,
+	[LogEvent] [nvarchar](max) NULL,
+	[UserName] [nvarchar](200) NULL,
+	[IP] [varchar](200) NULL,
+ CONSTRAINT [PK_Log] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
